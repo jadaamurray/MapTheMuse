f48fa733a10b179c36750d49247f609aabdf662f
Jada Murray <jada736@icloud.com>
2025-09-08 11:28:54 +0100

    housekeeping migrations

--- backend/MapTheMuseApi/Migrations/20250908102129_housekeeping.Designer.cs
+++ backend/MapTheMuseApi/Migrations/20250908102129_housekeeping.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using MapTheMuseApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MapTheMuseApi.Migrations
 {
     [DbContext(typeof(MapTheMuseContext))]
-    partial class MapTheMuseContextModelSnapshot : ModelSnapshot
+    [Migration("20250908102129_housekeeping")]
+    partial class housekeeping
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/MapTheMuseApi/Migrations/20250908102129_housekeeping.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MapTheMuseApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class housekeeping : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserMediaEngagements_Destinations_DestinationId",
+                table: "UserMediaEngagements");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DestinationId",
+                table: "UserMediaEngagements",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserMediaEngagements_Destinations_DestinationId",
+                table: "UserMediaEngagements",
+                column: "DestinationId",
+                principalTable: "Destinations",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserMediaEngagements_Destinations_DestinationId",
+                table: "UserMediaEngagements");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DestinationId",
+                table: "UserMediaEngagements",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserMediaEngagements_Destinations_DestinationId",
+                table: "UserMediaEngagements",
+                column: "DestinationId",
+                principalTable: "Destinations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
